c857c0345445241107e74ab31aa6721c386cab1c
Hung Nguyen <hungmanh.hust@gmail.com>
2017-08-12 23:09:13 +0700

    Bai 1 Playground

---
+++ Collection/Dictionary/PlaygroundDictionarySwift3.playground/Contents.swift
@@ -0,0 +1,44 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var responseMessages = [
+                        200: "OK",
+                        403: "Access forbidden",
+                        404: "File not found",
+                        500: "Internal server error"
+                        ]
+
+print(responseMessages[200])
+print(responseMessages[300])
+print("-------------------")
+
+
+let httpResponseCode = [200, 403, 301]
+
+for code in httpResponseCode {
+    if let message = responseMessages[code] {
+        print("\(code): \(message)")
+    }else{
+        print("Unknow response \(code)")
+    }
+}
+
+let records :[[AnyObject]] = [
+    [1 as AnyObject, "Nguyen" as AnyObject, "Hung" as AnyObject, "MT Soft ware!" as AnyObject, 0 as AnyObject],
+    [2 as AnyObject, "Nguyen" as AnyObject, "Hung" as AnyObject, "MT Soft ware!" as AnyObject, 10 as AnyObject],
+    [3 as AnyObject, "Nguyen" as AnyObject, "Hung" as AnyObject, "MT Soft ware!" as AnyObject, 20 as AnyObject],
+    [4 as AnyObject, "Nguyen" as AnyObject, "Hung" as AnyObject, "MT Soft ware!" as AnyObject, 30 as AnyObject],
+    [5 as AnyObject, "Nguyen" as AnyObject, "Hung" as AnyObject, "MT Soft ware!" as AnyObject, 40 as AnyObject],
+    [6 as AnyObject, "Nguyen" as AnyObject, "Hung" as AnyObject, "MT Soft ware!" as AnyObject, 50 as AnyObject],
+    
+                            ]
+
+let allSalaries = records.map{$0.last as! Int}
+print(allSalaries)
+
+let total = allSalaries.reduce(0, - )
+print(total)
+
+let goodPay = records.filter{($0.last as! Int) > 20}
+print(goodPay)
